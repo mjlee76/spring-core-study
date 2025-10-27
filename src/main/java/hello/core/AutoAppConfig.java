3fc97fca9951938e26b3d89fafd5e394c69685b7
mjlee76 <dlalswns0422@gmail.com>
2025-10-27 17:23:39 +0900

    25.10.27

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,14 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    // memoryMemberRepository는 이미 빈으로 자동등록되어있지만 같은 이름으로 수동으로 등록해보자
+    // 테스트는 실행이 된다 -> 수동으로 등록한게 우선으로 오버라이딩됨.
+    // 하지만 스프링 부트를 실행하면 이걸 오류로 처리해버림.
+    // 이렇게 오버라이딩 되도록하면 나중에 꼬일 수 있기 때문.
+    // 하지만 이 기능 사용하고 싶으면 application.properties에
+    // spring.main.allow-bean-definition-overriding=true 로 설정하면 실행 가능하다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,11 +15,29 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //의존관계 주입: 1. 생성자 주입 방법
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    //의존관게 주입: 2. 수정자 메서드 주입 방법
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+    //단, set으로 의존관계 주입시 필드의 final은 빼야함.
+
+    //의존관계 주입: 3. 필드 주입
+//    @Autowired private final MemberRepository memberRepository;
+//    @Autowired private final DiscountPolicy discountPolicy;
+    //권장되지 않음. 외부에서 변경이 불가능해서 테스트하기 힘들다는 치명적인 단점 존재.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
